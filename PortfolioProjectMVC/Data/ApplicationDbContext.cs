b01bc82b98ee4e874a07b4f4bce8a926e9a23927
Dominic Perrine <16063254@mstc.edu>
2023-07-03 22:20:30 -0500

    Adding DbContext

---
+++ PortfolioProjectMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioProjectMVC.Models;
+
+namespace PortfolioProjectMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
