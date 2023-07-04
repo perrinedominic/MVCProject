592fe4538566f95024ab1242ba349551abc78d21
Dominic Perrine <16063254@mstc.edu>
2023-07-03 23:07:50 -0500

    Added Controller for categories

--- PortfolioProjectMVC/Program.cs
+++ PortfolioProjectMVC/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PortfolioProjectMVC.Data;
+
 namespace PortfolioProjectMVC
 {
     public class Program
@@ -8,6 +11,9 @@ namespace PortfolioProjectMVC
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")
+                ));
 
             var app = builder.Build();
 
