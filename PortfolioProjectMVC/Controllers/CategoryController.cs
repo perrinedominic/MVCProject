6def32015c4a52d89ce8606ab2f60c7dc23106bc
Dominic Perrine <16063254@mstc.edu>
2023-08-13 16:44:36 -0500

    Toastr for notification handling.

--- PortfolioProjectMVC/Controllers/CategoryController.cs
+++ PortfolioProjectMVC/Controllers/CategoryController.cs
@@ -37,9 +37,10 @@ namespace PortfolioProjectMVC.Controllers
 
             if(ModelState.IsValid)
             {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully.";
+                return RedirectToAction("Index");
             }
 
             return View();
@@ -74,6 +75,7 @@ namespace PortfolioProjectMVC.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully.";
                 return RedirectToAction("Index");
             }
 
@@ -108,6 +110,7 @@ namespace PortfolioProjectMVC.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully.";
             return RedirectToAction("Index");
         }
     }
