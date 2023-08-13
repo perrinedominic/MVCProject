46f899672a4b41924ca555b2246c0f5f05a374ac
Dominic Perrine <16063254@mstc.edu>
2023-08-13 15:47:14 -0500

    Create category functionality and error handling.

--- PortfolioProjectMVC/Controllers/CategoryController.cs
+++ PortfolioProjectMVC/Controllers/CategoryController.cs
@@ -18,5 +18,31 @@ namespace PortfolioProjectMVC.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if(ModelState.IsValid)
+            {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
